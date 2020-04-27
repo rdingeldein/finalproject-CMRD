6b0a5446f204b8ba7490030a00c918e89bc6db01
Ruthie Dingeldein <ruthding@umich.edu>
2020-04-27 17:10:00 -0400

    Separate functions made

--- sophomore yr/2nd Semester/SI 206/Final-Project-CM-RD/finalproject.py
+++ sophomore yr/2nd Semester/SI 206/Final-Project-CM-RD/finalproject.py
@@ -19,37 +19,38 @@ import math
 
 #setting up database
 db_name = 'Population Analysis'
-path = os.path.dirname(os.path.abspath(__file__))
-conn = sqlite3.connect(path+'/'+db_name)
-cur = conn.cursor()
-
-#joining the databases 
-cur.execute('SELECT city, population_density, Salaries.ranking, Salaries.average_hourly, population FROM Populations INNER JOIN Salaries ON Populations.city = Salaries.citystate')
-rest = cur.fetchall()
-conn.commit()
+def set_up(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    cur.execute('SELECT city, population_density, Salaries.ranking, Salaries.average_hourly, population FROM Populations INNER JOIN Salaries ON Populations.city = Salaries.citystate')
+    rest = cur.fetchall()
+    conn.commit()
+    return rest
 
 #first figure 
-plt.ylabel('Hourly Salary')
-plt.xlabel('Population Density')
-plt.title("Relationship between uber/lyft salaires and population density")
-hourlysalaries = []
-popdensities = []
-for city in reversed(rest):
-    density = city[1].replace(",","")
-    popdensities.append(int(density))
-    sal = city[3].replace("$","")
-    hourlysalaries.append((sal))
-plt.scatter(popdensities, hourlysalaries, color='r')
+def figure_one():
+    plt.ylabel('Hourly Salary')
+    plt.xlabel('Population Density')
+    plt.title("Relationship between uber/lyft salaries and population density")
+    hourlysalaries = []
+    popdensities = []
+    for city in reversed(set_up(db_name)):
+        density = city[1].replace(",","")
+        popdensities.append(int(density))
+        sal = city[3].replace("$","")
+        hourlysalaries.append((sal))
+    plt.scatter(popdensities, hourlysalaries, color='r')
 
-plt.show()
+    plt.show()
 
 #relation between pop and salary  
 plt.ylabel('Hourly Salary')
 plt.xlabel('Population')
-plt.title("Relationship between uber/lyft salaires and population")
+plt.title("Relationship between uber/lyft salaries and population")
 hourlysalaries = []
 popdensities = []
-for city in reversed(rest):
+for city in reversed(set_up(db_name)):
     density = city[4].replace(",","")
     popdensities.append(int(density))
     sal = city[3].replace("$","")
@@ -60,75 +61,75 @@ plt.show()
 
 
 #average salary by city type 
-small_total = 0
-small_count = 0
-medium_total = 0
-medium_count = 0
-large_total = 0
-large_count = 0
-for city in rest:
-    if int(city[1].replace(',', '')) < 5000:
-        small_total += float(city[3][1:])
-        small_count += 1
-    elif int(city[1].replace(',', '')) < 10000:
-        medium_total += float(city[3][1:])
-        medium_count += 1
-    else:
-        large_total += float(city[3][1:])
-        large_count += 1
-small_average = small_total/small_count
-medium_average = medium_total/medium_count
-large_average = large_total/large_count
+def figure_two():
+    small_total = 0
+    small_count = 0
+    medium_total = 0
+    medium_count = 0
+    large_total = 0
+    large_count = 0
+    for city in set_up(db_name):
+        if int(city[1].replace(',', '')) < 5000:
+            small_total += float(city[3][1:])
+            small_count += 1
+        elif int(city[1].replace(',', '')) < 10000:
+            medium_total += float(city[3][1:])
+            medium_count += 1
+        else:
+            large_total += float(city[3][1:])
+            large_count += 1
+    small_average = small_total/small_count
+    medium_average = medium_total/medium_count
+    large_average = large_total/large_count
 
-#second figure
-plt.ylabel('Average Hourly Salary')
-plt.xlabel('City Size')
-plt.title("Relationship between uber/lyft salaries and city size")
+    #second figure
+    plt.ylabel('Average Hourly Salary')
+    plt.xlabel('City Size')
+    plt.title("Relationship between uber/lyft salaries and city size")
 
-size = ('Small', 'Medium', 'Large')
-y_pos = np.arange(len(size))
-hourly = [small_average, medium_average, large_average]
+    size = ('Small', 'Medium', 'Large')
+    y_pos = np.arange(len(size))
+    hourly = [small_average, medium_average, large_average]
 
-plt.bar(y_pos, hourly, align='center', alpha=0.5, color = 'b', edgecolor='blue')
-plt.xticks(y_pos, size)
-plt.show()
+    plt.bar(y_pos, hourly, align='center', alpha=0.5, color = 'b', edgecolor='blue')
+    plt.xticks(y_pos, size)
+    plt.show()
 
 #third figure
-topfive = []
-cur.execute('SELECT citystate FROM Salaries')
-topthree = cur.fetchall()
-topthree = topthree[0:3]
+def figure_three(cur, conn):
+    cur.execute('SELECT citystate FROM Salaries')
+    topthree = cur.fetchall()
+    topthree = topthree[0:3]
 
+    #map plotting that works
+    fig = plt.figure(figsize=(7, 7))
+    m = Basemap(projection='lcc', resolution=None,
+                width=8E6, height=8E6, 
+                lat_0=45, lon_0=-100,)
+    m.etopo(scale=0.5, alpha=0.5)
 
-#map plotting that works
-fig = plt.figure(figsize=(7, 7))
-m = Basemap(projection='lcc', resolution=None,
-            width=8E6, height=8E6, 
-            lat_0=45, lon_0=-100,)
-m.etopo(scale=0.5, alpha=0.5)
-
-# Map (long, lat) to (x, y) for plotting
-x, y = m(-122.3, 39)
-plt.plot(x, y, 'ok', markersize=3)
-plt.text(x, y, topthree[0][0], fontsize=8)
-#second
-x, y = m(-122.3, 47)
-plt.plot(x, y, 'ok', markersize=3)
-plt.text(x, y, topthree[1][0], fontsize=8)
-#third
-x, y = m(-121, 36)
-plt.plot(x, y, 'ok', markersize=3)
-plt.text(x, y, topthree[2][0], fontsize=8)
-plt.show()
+    # Map (long, lat) to (x, y) for plotting
+    x, y = m(-122.3, 39)
+    plt.plot(x, y, 'ok', markersize=3)
+    plt.text(x, y, topthree[0][0], fontsize=8)
+    #second
+    x, y = m(-122.3, 47)
+    plt.plot(x, y, 'ok', markersize=3)
+    plt.text(x, y, topthree[1][0], fontsize=8)
+    #third
+    x, y = m(-121, 36)
+    plt.plot(x, y, 'ok', markersize=3)
+    plt.text(x, y, topthree[2][0], fontsize=8)
+    plt.show()
 
 #writing to a csv
 with open('data.txt', 'w', newline='') as file:
     writer = csv.writer(file)
     labels = ("City and State", "Population Density", "Ranking", "Hourly Salary", "Population")
     writer.writerow(labels)
-    for entries in rest:
+    for entries in set_up(db_name):
         writer.writerow(entries)
         file.flush()
-file.close()
+    file.close()
 
 
--- sophomore yr/2nd Semester/SI 206/Final-Project-CM-RD/gettingdata.py
+++ sophomore yr/2nd Semester/SI 206/Final-Project-CM-RD/gettingdata.py
@@ -20,121 +20,128 @@ import math
 
 #gathering data for populations per city
 article_url = 'https://www.governing.com/gov-data/population-density-land-area-cities-map.html'
-p = requests.get(article_url)
-page = p.text
-soup = BeautifulSoup(page, 'html.parser')
-city_summaries = []
-for city_data in soup.find_all('tr'):
-    city_data = city_data.text
-    city_data = city_data.strip()
-    city_data = city_data.split('\n')
-    city = city_data[0]
-    population_density = city_data[1]
-    population = city_data[2]
-    land_area = city_data[3]
-    summary = (city, population_density, population, land_area)
-    city_summaries.append(summary)
+def gather_data(article_url):
+    p = requests.get(article_url)
+    page = p.text
+    soup = BeautifulSoup(page, 'html.parser')
+    city_summaries = []
+    for city_data in soup.find_all('tr'):
+        city_data = city_data.text
+        city_data = city_data.strip()
+        city_data = city_data.split('\n')
+        city = city_data[0]
+        population_density = city_data[1]
+        population = city_data[2]
+        land_area = city_data[3]
+        summary = (city, population_density, population, land_area)
+        city_summaries.append(summary)
+    return city_summaries
 
 #setting up database
 db_name = 'Population Analysis'
-path = os.path.dirname(os.path.abspath(__file__))
-conn = sqlite3.connect(path+'/'+db_name)
-cur = conn.cursor()
+def set_up(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
 
 #setting up Populations Table
-
-cur.execute("CREATE TABLE IF NOT EXISTS Populations (city TEXT PRIMARY KEY, population_density INTEGER, population INTEGER, land_area INTEGER)")
-for i in range(30):
-    cur.execute("SELECT * FROM Populations")
-    index = len(cur.fetchall())
-    if index <= len(city_summaries):
-        for city in city_summaries[index:index+20]:
-            cur.execute("INSERT OR IGNORE INTO Populations (city, population_density, population, land_area) VALUES (?,?,?,?)", city)
-    i += 1
-    conn.commit()
+def population_table(article_url, cur, conn):
+    cur.execute("CREATE TABLE IF NOT EXISTS Populations (city TEXT PRIMARY KEY, population_density INTEGER, population INTEGER, land_area INTEGER)")
+    for i in range(30):
+        cur.execute("SELECT * FROM Populations")
+        index = len(cur.fetchall())
+        if index <= len(gather_data(article_url)):
+            for city in gather_data(article_url)[index:index+20]:
+                cur.execute("INSERT OR IGNORE INTO Populations (city, population_density, population, land_area) VALUES (?,?,?,?)", city)
+        i += 1
+        conn.commit()
 
 
 
 #setting up Type Table
-
-cur.execute("CREATE TABLE IF NOT EXISTS Type (city TEXT PRIMARY KEY, type TEXT)")
-for i in range(30):
-    cur.execute("SELECT * FROM Type")
-    index = len(cur.fetchall())
-    if index <= len(city_summaries):
-        for city in city_summaries[index:index+20]:
-            if int(city[1].replace(',', '')) < 5000:
-                cur.execute("INSERT OR IGNORE INTO Type (city, type) VALUES (?, ?)", (city[0], 'Small'))
-            elif int(city[1].replace(',', '')) < 10000:
-                cur.execute("INSERT OR IGNORE INTO Type (city, type) VALUES (?, ?)", (city[0], 'Medium'))
-            else:
-                cur.execute("INSERT OR IGNORE INTO Type (city, type) VALUES (?, ?)", (city[0], 'Large'))
-    i += 1
-    conn.commit()
+def type_table(article_url, cur, conn):
+    cur.execute("CREATE TABLE IF NOT EXISTS Type (city TEXT PRIMARY KEY, type TEXT)")
+    for i in range(30):
+        cur.execute("SELECT * FROM Type")
+        index = len(cur.fetchall())
+        if index <= len(gather_data(article_url)):
+            for city in gather_data(article_url)[index:index+20]:
+                if int(city[1].replace(',', '')) < 5000:
+                    cur.execute("INSERT OR IGNORE INTO Type (city, type) VALUES (?, ?)", (city[0], 'Small'))
+                elif int(city[1].replace(',', '')) < 10000:
+                    cur.execute("INSERT OR IGNORE INTO Type (city, type) VALUES (?, ?)", (city[0], 'Medium'))
+                else:
+                    cur.execute("INSERT OR IGNORE INTO Type (city, type) VALUES (?, ?)", (city[0], 'Large'))
+        i += 1
+        conn.commit()
 
 #gathering data for populations per city
 article_url = 'https://www.businessinsider.com/highest-earning-cities-uber-lyft-2019-12'
-p = requests.get(article_url)
-page = p.text
-soup = BeautifulSoup(page, 'html.parser')
-salaries_sum = []
-headers = soup.find_all('h2', class_ = 'slide-title-text')
+def gather_data2(article_url):
+    p = requests.get(article_url)
+    page = p.text
+    soup = BeautifulSoup(page, 'html.parser')
+    salaries_sum = []
+    headers = soup.find_all('h2', class_ = 'slide-title-text')
 
-for city_data in headers:
-    city_data = city_data.text
-    city_data = city_data.strip()
-    city_data = city_data.split()
+    for city_data in headers:
+        city_data = city_data.text
+        city_data = city_data.strip()
+        city_data = city_data.split()
 
-    if city_data[2]== 'York,':
-        ranking = city_data[0]
-        ranking = ranking.replace(".", "")
-        citypart1 = city_data[1]
-        citypart2 = city_data[2]
-        #citypart2 = citypart2.replace(",", "")
-        city = citypart1 + " " + citypart2
-        statepart1 = city_data[3]
-        statepart2 = city_data[4]
-        citystate = city + " " +statepart1 + " " + statepart2
-        
-        average_hourly = city_data[6]
+        if city_data[2]== 'York,':
+            ranking = city_data[0]
+            ranking = ranking.replace(".", "")
+            citypart1 = city_data[1]
+            citypart2 = city_data[2]
+            #citypart2 = citypart2.replace(",", "")
+            city = citypart1 + " " + citypart2
+            statepart1 = city_data[3]
+            statepart2 = city_data[4]
+            citystate = city + " " +statepart1 + " " + statepart2
+            
+            average_hourly = city_data[6]
 
-    elif city_data[1]== 'Charlotte,':
-        ranking = city_data[0]
-        ranking = ranking.replace(".", "")
-        city = city_data[1]
-        statepart1 = city_data[2]
-        statepart2 = city_data[3]
-        citystate = city + " " + statepart1 + " " + statepart2 
-        average_hourly = city_data[5]
+        elif city_data[1]== 'Charlotte,':
+            ranking = city_data[0]
+            ranking = ranking.replace(".", "")
+            city = city_data[1]
+            statepart1 = city_data[2]
+            statepart2 = city_data[3]
+            citystate = city + " " + statepart1 + " " + statepart2 
+            average_hourly = city_data[5]
 
-    elif len(city_data) == 6:
-        ranking = city_data[0]
-        ranking = ranking.replace(".", "")
-        citypart1 = city_data[1]
-        citypart2 = city_data[2]
-        #citypart2 = citypart2.replace(",", "")
-        city = citypart1 + " " + citypart2
-        citystate = city + " " + city_data[3]
-        average_hourly = city_data[5]
+        elif len(city_data) == 6:
+            ranking = city_data[0]
+            ranking = ranking.replace(".", "")
+            citypart1 = city_data[1]
+            citypart2 = city_data[2]
+            #citypart2 = citypart2.replace(",", "")
+            city = citypart1 + " " + citypart2
+            citystate = city + " " + city_data[3]
+            average_hourly = city_data[5]
 
-    else:
-        ranking = city_data[0]
-        ranking = ranking.replace(".", "")
-        city = city_data[1]
-        #city = city.replace(",", "")
-        citystate = city + " " +city_data[2]
-        average_hourly = city_data[4]
-        
-    summary = (ranking, citystate, average_hourly)
-    salaries_sum.append(summary)
+        else:
+            ranking = city_data[0]
+            ranking = ranking.replace(".", "")
+            city = city_data[1]
+            #city = city.replace(",", "")
+            citystate = city + " " +city_data[2]
+            average_hourly = city_data[4]
+            
+        summary = (ranking, citystate, average_hourly)
+        salaries_sum.append(summary)
+    return salaries_sum
 
 
-cur.execute("CREATE TABLE IF NOT EXISTS Salaries (ranking INTEGER PRIMARY KEY, citystate TEXT, average_hourly TEXT)")
-for i in range(2):
-    cur.execute("SELECT * FROM Salaries")
-    index = len(cur.fetchall())
-    if index <= len(salaries_sum):
-        for salary in salaries_sum[index:index+16]:
-            cur.execute("INSERT OR IGNORE INTO Salaries (ranking, citystate, average_hourly) VALUES (?,?,?)", ranking)
-    i += 1
-    conn.commit()
+def salaries_table(article_url, cur, conn):
+    cur.execute("CREATE TABLE IF NOT EXISTS Salaries (ranking INTEGER PRIMARY KEY, citystate TEXT, average_hourly TEXT)")
+    for i in range(2):
+        cur.execute("SELECT * FROM Salaries")
+        index = len(cur.fetchall())
+        if index <= len(gather_data2(article_url)):
+            for salary in gather_data2(article_url)[index:index+16]:
+                cur.execute("INSERT OR IGNORE INTO Salaries (ranking, citystate, average_hourly) VALUES (?,?,?)", ranking)
+        i += 1
+        conn.commit()
